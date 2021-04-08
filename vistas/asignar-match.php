afbf095630afffb477cafa97f58d08318cb29119
Luis Yael Bautista Zaldivar <yael.bz@outlook.com>
2021-04-08 13:44:47 -0500

    Modificación plantilla V1

--- vistas/asignar-match.php
+++ vistas/asignar-match.php
@@ -2,74 +2,24 @@
 // Hace una validación de que ehaya un usuario en sesión.
 session_start();
 if ($_SESSION["s_usuario"] === "null") {
-    header("Location: ../index.php");
+    header("Location: ../log-in.php");
 }
 ?>
-
+<?php require_once "./parte_superior.php" ?>
+<!-- INICIO CONTENIDO PRINCIPAL -->
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="../estilos-am.css">
-
+    <title>Coordinador</title>
 </head>
-
 <body>
     <div class="content-fluid">
-        <div class="d-flex align-items-start" id="menu">
-
-            <div class="tab-content-fluid" id="v-pills-tabContent">
-
-                <div class="tab-pane fade show active p-4" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">
-
-                    <div class="row p-4" id="graficas-am">
-                        <div class="col">
-                            <img src="../static/img/pie-chart.png" alt="" width="100px">
-                            <h2>Activación 1</h2>
-                        </div>
-                        <div class="col">
-                            <img src="../static/img/pie-chart.png" alt="" width="100px">
-                            <h2>Activación 2</h2>
-                        </div>
-                        <div class="col">
-                            <img src="../static/img/pie-chart.png" alt="" width="100px">
-                            <h2>Activación 3</h2>
-                        </div>
-                    </div>
-
-                    <div class="row justify-content-md-center p-4" id="tabla-am">
-                        <div class="col-md-auto">
-                            <table class="tabla-coord" id="tabla-c">
-                                <tr>
-                                    <td>Coordinador</td>
-                                    <td>Match Asignados</td>
-                                    <td></td>
-                                </tr>
-                                <?php
-                                $res = mysqli_query($con, "SELECT * FROM emocion");
-                                if ($rec = mysqli_fetch_array($res)) {
-                                    echo '<tr><td>' . $rec["Id_emocion"] . '</td><td>' . $rec["emocion"] . '</td><td><button>Editar</button></td</tr>';
-                                }
-                                ?>
-                            </table>
-                        </div>
-                    </div>
-                </div>
-
-
-                <div class="tab-pane fade" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab">
-
-                </div>
-            </div>
-        </div>
+    
     </div>
 </body>
-
 </html>
+<!-- FIN DEL CONTENIDO PRINCIPAL -->
+<?php require_once "./parte_inferior.php" ?>
--- vistas/index.php
+++ vistas/index.php
@@ -6,8 +6,53 @@ if ($_SESSION["s_usuario"] === "null") {
 }
 ?>
 <?php require_once "./parte_superior.php" ?>
-    <!-- INICIO CONTENIDO PRINCIPAL -->
+<!-- INICIO CONTENIDO PRINCIPAL -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Super Administrador</title>
+</head>
+<body>
+    <div class="content-fluid">
+        <!-- Consultar -->
+        <div class="row justify-content-md-center p-4">
+            <div class="col-md-auto"><input class="form-control" type="text" placeholder="Id Match"></div>
+            <div class="col-md-auto"><input class="form-control" type="text" placeholder="Empresa"></div>
+            <div class="col-md-auto"><input class="form-control" type="text" placeholder="Coordinador"></div>
+            <div class="col-md-auto"><button type="submit" class="btn btn-primary">Consultar</button></div>
+        </div>
 
-    <!-- FIN DEL CONTENIDO PRINCIPAL -->
+        <!-- Graficación -->
+        <div class="row justify-content-md-center p-4">
+            <div class="col-md-auto">
+                <img src="../static/img/pie-chart.png" alt="" width="300px">
+            </div>
+        </div>
 
+        <!-- Paginación -->
+        <div class="row justify-content-md-center p-4">
+            <div class="col-md-auto">
+                <table border=3>
+                    <tr>
+                        <td>Id_Match</td><td>Nombre PAM</td><td>Nombre Voluntario</td><td>Nombre coordinador</td><td>Empresa</td>
+                    </tr>
+                    <tr>
+                        <td>1</td><td>Prueba uno PAM</td><td>Prueba uno V</td><td>Prueba uno C</td><td>Prueba uno E</td>
+                    </tr>
+                    <tr>
+                        <td>2</td><td>Prueba dos PAM</td><td>Prueba dos V</td><td>Prueba dos C</td><td>Prueba dos E</td>
+                    </tr>
+                    <tr>
+                        <td>3</td><td>Prueba tres PAM</td><td>Prueba tres V</td><td>Prueba tres C</td><td>Prueba tres E</td>
+                    </tr>
+                </table>
+            </div>            
+        </div>
+    </div>
+</body>
+</html>
+<!-- FIN DEL CONTENIDO PRINCIPAL -->
 <?php require_once "./parte_inferior.php" ?>
